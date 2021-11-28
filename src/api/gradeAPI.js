d6bcede18101475aa28d230fa34a0f4957b4b50f
Hà Minh Cường <55825733+haminhcuong2k@users.noreply.github.com>
2021-11-28 21:00:41 +0700

    Create gradeAPI.js

---
+++ src/api/gradeAPI.js
@@ -0,0 +1,59 @@
+import axios from 'axios';
+
+require('dotenv').config();
+
+export async function getAllGrade(courseId) {
+  const url = `${process.env.REACT_APP_BASE_URL}/courses/${courseId}/grades`;
+  const token = localStorage.getItem('token');
+
+  const response = await axios.get(url, {
+    headers: {
+      'Content-Type': 'application/json',
+      'x-access-token': token,
+    },
+  });
+
+  return response.data;
+}
+
+export async function createGrade(courseId, newGrade) {
+  const url = `${process.env.REACT_APP_BASE_URL}/courses/${courseId}/grades`;
+  const token = localStorage.getItem('token');
+
+  const response = await axios.post(url, newGrade, {
+    headers: {
+      'Content-Type': 'application/json',
+      'x-access-token': token,
+    },
+  });
+
+  return response.data;
+}
+
+export async function updateGrade(courseId, gradeId, newGrade) {
+  const url = `${process.env.REACT_APP_BASE_URL}/courses/${courseId}/grades/${gradeId}`;
+  const token = localStorage.getItem('token');
+
+  const response = await axios.put(url, newGrade, {
+    headers: {
+      'Content-Type': 'application/json',
+      'x-access-token': token,
+    },
+  });
+
+  return response.data;
+}
+
+export async function deleteGrade(courseId, gradeId) {
+  const url = `${process.env.REACT_APP_BASE_URL}/courses/${courseId}/grades/${gradeId}`;
+  const token = localStorage.getItem('token');
+
+  const response = await axios.delete(url, {
+    headers: {
+      'Content-Type': 'application/json',
+      'x-access-token': token,
+    },
+  });
+
+  return response.data;
+}
